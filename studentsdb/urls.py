763a2e3aa37e5a5dc314c249b19d3540f10b0b59
”Serhii <sergvh@gmail.com>
2018-07-31 20:35:02 +0300

    some url tegs added

--- studentsdb/urls.py
+++ studentsdb/urls.py
@@ -13,9 +13,31 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
+from students.views import students_list, students_add, students_edit, students_delete
+from students.views import groups_list, groups_add, groups_edit, groups_delete
 
 urlpatterns = [
+
+    # Students urls
+
+    url(r'^$', students_list, name='home'),
+
+    url(r'^students/add/$', students_add, name='students_add'),
+
+    url(r'^students/(?P<sid>\d+)/edit/$', students_edit, name='students_edit'),
+
+    url(r'^students/(?P<sid>\d+)/delete/$', students_delete, name='students_delete'),
+
+    # Groups urls
+    url(r'^groups/$', groups_list, name='groups'),
+
+    url(r'^groups/add/$', groups_add , name='groups_add'),
+
+    url(r'^groups/(?P<gid>\d+)/edit/$', groups_edit, name='groups_edit'),
+
+    url(r'^groups/(?P<gid>\d+)/delete/$', groups_delete, name='groups_delete'),
+
     url(r'^admin/', admin.site.urls),
 ]

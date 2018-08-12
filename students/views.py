967fdfc8b7cec7c698619a152805a5e7002fed17
”Serhii <sergvh@gmail.com>
2018-08-12 18:17:57 +0300

    groups list added and dynamized

--- students/views.py
+++ students/views.py
@@ -7,21 +7,21 @@ from django.http import HttpResponse
 
 def students_list(request):
     students = (
-        {'i': 1,
+        {'id': 1,
         'first_name': u'Максим',
         'last_name': u'Потапенко',
         'ticket': 235,
-        'image': 'img / me.jpeg'},
+        'image': 'img/1.jpg'},
         {'id': 2,
         'first_name': u'Олег',
         'last_name': u'Синюткін',
         'ticket': 2123,
-        'image': 'img / piv.png'},
+        'image': 'img/2.jpg'},
         {'id': 3,
          'first_name': u'Андрійко',
          'last_name': u'Задовий',
          'ticket': 5332,
-         'image': 'img / piv.png'},
+         'image': 'img/3.jpg'},
     )
     return render(request, 'students/students_list.html', {'students': students})
 
@@ -36,7 +36,19 @@ def students_delete(request, sid):
 
 # Views for Groups
 def groups_list(request):
-    return render(request, 'students/groups_list.html', {})
+    groups = (
+        {'id' : 1,
+         'name' : u'МтМ-21',
+         'head' : u'Потапенко Максим'},
+        {'id': 2,
+         'name': u'МтМ-22',
+         'head': u'Синюткін Олег'},
+        {'id': 3,
+         'name': u'МтМ-23',
+         'head': u'Задовий Андрій'},
+    )
+
+    return render(request, 'students/groups_list.html', {'groups' : groups})
 
 def groups_add(request):
     return HttpResponse('<h1>Group Add Form</h1>')
